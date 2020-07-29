b69f16c1fa97a649c87abab503dd189d46ee10d5
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-07-29 18:23:29 -0500

    installed npm, downloaded inquire, created question prompt

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+    
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(portfolioData);
 
-fs.writeFile('index.html', generatePage(name, github) , err => {
-    if (err) throw err;
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github) , err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
